efa26fb07b622a89826f45e9da0b39c7ab0d0c76
Angga Priantoko <anggapriantoko2000@gmail.com>
2021-03-04 20:44:48 +0700

    add reservation page

---
+++ reservasi_online.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Developing by Ahmad Navis Zarkasy (TOS IT Development) -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Harga Paket Outbound Fun Adventure di TOS ADVENTURE Pacet Mojokerto Jawa Timur</title>
+    <meta name="description" content="Rasakan sensasi kegiatan outbound yang fun dan fresh dipadukan dengan kegiatan adventure rafting di Pacet Mojokerto. Hubungi kami segera untuk infonya" />
+    <meta name="keywords" content="rafting, outbound, rafting pacet, rafting mojokerto, rafting jawa timur, outbound pacet, outbound mojokerto, outbound jawa timur, rafting terdekat, outbound terdekat, wisata pacet, wisata mojokerto, wisata jawa timur"/>
+    <meta name="robots" content="index, follow">
+    <meta name="googlebot" content="index, follow">
+    <meta name="language" content="indonesia">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="shortcut icon" type="image/png" href="img/new_tos.png"/>
+    
+</head>
+
+<body>
+
+<?php include "header.php"?>
+
+<section id="kosong">
+
+</section>
+
+<?php include "footer.php"?>
+
+</body>
+</html>
